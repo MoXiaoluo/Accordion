630aa4365bcc47ee6c1a793c92aebf108cf80645
JustDo <771595316@qq.com>
2016-10-09 22:59:17 +0800

    use another function to achieve accordion
    use another function to achieve accordion

--- js/accordion.js
+++ js/accordion.js
@@ -1,6 +1,13 @@
 $(function(){
   $('.accordion-header').on('click',function(){
-    $('.accordion-body').addClass('hidden');
-    $(this).next().removeClass('hidden');
+
+    $(".accordion-body").not($(this).next()).slideUp(400);
+    $(this).next().slideToggle(400);
+    // if(!$(this).next().hasClass('hidden')){
+    //   $(this).next().addClass('hidden');
+    // }else{
+    //   $('.accordion-body').addClass('hidden');
+    //   $(this).next().removeClass('hidden');
+    // }
   })
 })
